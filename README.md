52843a33adff623bcc6bd1edd8b07eb5c45d206b
Demon <kristyndcb@gmail.com>
2022-05-14 22:51:10 -0600

    User input is being saved in local storeage.

--- README.md
+++ README.md
@@ -12,7 +12,14 @@ SO THAT I can manage my time effectively
 ## What I Learned
  
 I focused on incorporating other libraries to create the work day scheduler. These included:
--moment.js
+
+
+-Moment.js
+
 -jQuery
+
 -Bootstrap
-Working with jQuery can have quite a few benefits as compared to vanilla JS such as being able to call the object keys a lot more simplicitly.
+
+-Googlefonts
+
+Working with jQuery can have quite a few benefits as compared to vanilla JS such as being able to call the object keys a lot more simplicitly.But overall this assignment's incorporation of multiple libraries into one html was a great excersise.
--- script.js
+++ script.js
@@ -1,5 +1,76 @@
 //using moment.js
-var today = moment();
-//this will display in jumbotron in the format i specified right at success it will stick to the page and won't change unless page is reloaded. 
-$("#currentDay").text(today.format("MMMM Do, YYYY"));
+var moment = moment();
+var btn = document.querySelector("#mybutton");
+
+$("#currentDay").text(moment.format("MMMM Do, YYYY"));
+
+//save input text in local storeage when button is clicked, done for each button bc input is different. 
+$("#button1").click(function (event) {
+    event.preventDefault();
+    var text = document.querySelector("#input1").value;
+    localStorage.setItem("9am", text);
+});
+$("#button2").click(function (event) {
+  event.preventDefault();
+  var text = document.querySelector("#input2").value;
+  localStorage.setItem("10am", text);
+});
+$("#button3").click(function (event) {
+  event.preventDefault();
+  var text = document.querySelector("#input3").value;
+  localStorage.setItem("11am", text);
+});
+$("#button4").click(function (event) {
+  event.preventDefault();
+  var text = document.querySelector("#input4").value;
+  localStorage.setItem("Noon", text);
+});
+$("#button5").click(function (event) {
+  event.preventDefault();
+  var text = document.querySelector("#input5").value;
+  localStorage.setItem("1pm", text);
+});
+$("#button6").click(function (event) {
+  event.preventDefault();
+  var text = document.querySelector("#input6").value;
+  localStorage.setItem("2pm", text);
+});
+$("#button7").click(function (event) {
+  event.preventDefault();
+  var text = document.querySelector("#input7").value;
+  localStorage.setItem("3pm", text);
+});
+$("#button8").click(function (event) {
+  event.preventDefault();
+  var text = document.querySelector("#input8").value;
+  localStorage.setItem("4pm", text);
+});
+$("#button9").click(function (event) {
+  event.preventDefault();
+  var text = document.querySelector("#input9").value;
+  localStorage.setItem("5pm", text);
+});
+ 
+
+
+
+//SAVING IN LOCAL STOREAGE 
+
+// $("#input-01").val(localStorage.getItem("hour-01"))//do this for each input 
+// function showNotice() {
+//     $(".save-notice").show()
+//     setTimeout(function () {
+//         $(#savenotice).hide()
+//     }, 1000);
+// }
+//id for every button id for every input 
+// $("#button-01").click(function () {
+//     var inputVal = $(this).siblings('input'.val());
+//     console.log(inputVal);
+//     localStorage.setItem("hour-01", inputVal)
+   // showNotice();
+// })
+
+
+
 
